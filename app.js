2b40298563a29546cfb1a8c3a03540cfdb7bd904
my name <blueshaperd3@gmail.com>
2024-03-25 23:49:42 +0500

    table in progress

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-for (var a = 1; a <= 100; a = a+10){
-    for(var b = a; b < a + 10; b++){
-document.write(b + " ");
+// Table
+// for (var a = 1; a <= 100; a = a+10){
+//     for(var b = a; b < a + 10; b++){
+// document.write(b + " ");
+// }
+// document.write( "<br>");
+// }
+
+
+for (var a = 1; a <= 100; a = a+2){
+    for(var b = a; b < a +1; b++)
+    document.write(a + " ");
 }
-document.write( "<br>");   // para que salga
-}
+// document.write("<br>")
