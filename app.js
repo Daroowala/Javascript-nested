cc485437b0b8678b52760528259f93401450cfb4
my name <blueshaperd3@gmail.com>
2024-03-26 17:15:57 +0500

    even table

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
 //     for(var b = a; b < a + 10; b++){
 // document.write(b + " ");
 // }
-// document.write( "<br>");
+// document.write("<br>");
 // }
 
 
-for (var a = 1; a <= 100; a = a+2){
-    for(var b = a; b < a +1; b++)
-    document.write(a + " ");
+
+for (var a = 1; a <= 50; a = a+2){
+    for (var b = a; b < a+10; b++){
+    document.write(b + " ");
 }
-// document.write("<br>")
+document.write("<br>")
+}
+   
